b76cf16ba0d1ef5ad2fa228c6382ee0ffd33f283
Ishan Sharma <143828488+ishan1274@users.noreply.github.com>
2024-08-10 07:50:22 +0530

    Time: 19 ms (69.55%), Space: 27.6 MB (60.38%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,8 +3,11 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k%n;
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+
+        reverse(nums.begin(), nums.end()); // reverses the entire array
+
+        reverse(nums.begin(), nums.begin() + k); // reverses the first k elements
+
+        reverse(nums.begin() + k, nums.end()); // reverses the elements after k elements
     }
 };
