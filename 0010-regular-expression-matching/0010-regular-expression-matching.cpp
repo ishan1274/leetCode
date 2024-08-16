9bbf973ca4030725d1c44adcd5fa6f2e64ab89eb
Ishan Sharma <143828488+ishan1274@users.noreply.github.com>
2024-08-16 09:17:53 +0530

    Time: 2 ms (75.11%), Space: 9.9 MB (18.13%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int n = s.length(), m = p.length();
-        bool dp[n+1][m+1];
-        memset(dp, false, sizeof(dp));
-        dp[0][0] = true;
-        
-        for(int i=0; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(p[j-1] == '*'){
-                    dp[i][j] = dp[i][j-2] || (i > 0 && (s[i-1] == p[j-2] || p[j-2] == '.') && dp[i-1][j]);
-                }
-                else{
-                    dp[i][j] = i > 0 && dp[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '.');
+         int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true; // Both strings are empty
+
+        // Initialize dp[0][j]
+        for (int j = 2; j <= n; ++j) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2]; // Assume 0 occurrence of preceding element
+                    if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
                 }
             }
         }
-        
-        return dp[n][m];
+
+        return dp[m][n];
     }
 };
